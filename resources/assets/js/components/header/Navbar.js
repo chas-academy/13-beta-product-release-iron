c432ffe85b988bfccbe15d45bc342f4584065b3f
Pontus Särland <pontus.sarland@chasacademy.se>
2018-04-13 14:43:55 +0200

    initial template setup

---
+++ resources/assets/js/components/header/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './navbar.css';
+
+ class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi im a Navbar</h1>
+            </div>
+        );
+    }
+}
+export default Navbar;
+
+//TODO: This is an example
