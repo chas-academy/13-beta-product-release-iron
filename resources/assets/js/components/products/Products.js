76367a7a418b4df1dd6d13cbe4841306460101c6
Keejv <kaveh.haddad@chasacademy.se>
2018-04-20 11:00:46 +0200

    Created product component

---
+++ resources/assets/js/components/products/Products.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Bottle from './JungleGinger.png';
+import "./prod.css"
+
+class Products extends Component {
+
+    render() {
+        return (
+            <div className="background">
+                <div className="productSite">
+                    <div className="productInfo">
+                        <h2 className="title is-1">Mega Product</h2>
+                        <h3 className="title is-3">Loui Jama - Mega Mate</h3>
+                        <h4 className="title is-6">Brukar du gå hem tidigt när ni är ute och festar? vill du drycka mega god juice din mega blatte?</h4>
+                    </div>
+                    <div className="jungelImg">
+                        <img src={Bottle} />
+                    </div>
+                </div>
+            </div>
+        );
+        
+    }
+}
+export default Products;
