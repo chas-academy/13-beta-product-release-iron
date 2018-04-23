0fcb748af7051e616065bdbe6dfdddb86accd949
Ida Englund <ida.englund@chasacademy.se>
2018-04-23 15:54:12 +0200

    small changes

--- resources/assets/js/components/Index.js
+++ resources/assets/js/components/Index.js
@@ -1,34 +1,21 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Navbar from "./header/Navbar.js";
 import Resellers from "./resellers/index.js";
 import Vision from "./vision/Vision.js";
+import Header from "./header/Header.js"
 
 export default class Index extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Navbar />
         <Vision />
         <Resellers />
       </div>
     );
   }
-=======
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Header from './header/Header.js';
-
-export default class Index extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-            </div>
-        );
-    }
->>>>>>> First frontend-code with menu, log  and background
 }
 
 if (document.getElementById("app")) {
