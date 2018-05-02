b8943a0fc69f7aac128d6906bf28589cfe27b9b1
Keejv <kaveh.haddad@chasacademy.se>
2018-05-02 14:38:56 +0200

    Update gitignore to re-include built assets for deploy
    - Remove unused image import in component and corresponding stylesheet
    - Update blade template to contain the CSRF token meta-tag (no more errors)

--- resources/assets/js/components/events/index.js
+++ resources/assets/js/components/events/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import eventsBackground from '../images/megaMate_eventsbg.png';
+
 import '../styles/styles.css';
 
  class Events extends Component {
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <title>{{ config('app.name', 'Mega Mate') }}</title>
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
