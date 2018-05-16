70575e83e8aa1fc755bb50b299fb3aa295d3efde
Tokidaze <hosseinalali@chasacademy.se>
2018-05-16 16:27:46 +0200

    change to uppercase for components, resolve issue with padding

--- resources/assets/js/components/Index.js
+++ resources/assets/js/components/Index.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';
-import Vision from './Vision';
-import ProductList from './Productlist';
-import EventsList from './Eventslist';
-import Resellers from './Resellers';
-import Footer from './Footer';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Vision from "./Vision";
+import ProductList from "./Productlist";
+import EventsList from "./Eventslist";
+import Resellers from "./Resellers";
+import Footer from "./Footer";
 
 export default class Index extends Component {
-	render() {
-		return (
-			<div>
-				<Header />
-				<Vision />
-				<ProductList />
-				<EventsList />
-				<Resellers />
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Vision />
+        <ProductList />
+        <EventsList />
+        <Resellers />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 if (document.getElementById("app")) {
-	ReactDOM.render(<Index />, document.getElementById("app"));
+  ReactDOM.render(<Index />, document.getElementById("app"));
 }
