1079cb5d1d98bc3c645c0bdf94f2fc3524746566
Pontus Särland <pontus.sarland@chasacademy.se>
2018-05-17 15:24:33 +0200

    added a widget folder in app

---
+++ app/Widgets/Vouchers.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Widgets;
+
+use Arrilot\Widgets\AbstractWidget;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+
+class Vouchers extends AbstractWidget
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Voucher::count();
+        $string = trans_choice('voyager::dimmer.post', $count);
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-treasure-open',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => 'Vouchers',
+                'link' => route('voyager.vouchers.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+}
