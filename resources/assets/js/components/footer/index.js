d156dc4b760326bc0d0d91a3aa7658878fc310ed
Tokidaze <hosseinalali@chasacademy.se>
2018-05-23 13:42:03 +0200

    remove old video, add new video

--- resources/assets/js/components/footer/index.js
+++ resources/assets/js/components/footer/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import '../styles/styles.css';
-import './styles.css';
-import NewsletterForm from '../newsletterForm';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "../styles/styles.css";
+import "./styles.css";
+import NewsletterForm from "../newsletterForm";
 
 class Footer extends Component {
   constructor() {
@@ -12,35 +12,46 @@ class Footer extends Component {
     };
   }
 
-
-	render() {
-		return (
-			<div>
-				<footer className="footer" id="contact">
-					
-					<div className="container footer-container">
-						<div className="content">
-							<p id="footer-title" className="footer-title fadeIn">Contact Us</p>
-							<p className="footer-p fadeIn">info@mega-mate.com</p>
-						</div>
-						<div className="content">
-							<a className="logo-link" href="https://www.facebook.com/megamate.sodafactory/" target="_blank">
-								<p className="footer-logo fadeinLeft">Facebook</p>
-							</a>
-							<a className="logo-link" href="https://www.instagram.com/mega_mate/" target="_blank">
-							<p className="footer-logo fadeinLeft">Instagram</p>
-							</a>
-							<a className="logo-link" href="https://www.sodafactory.se" target="_blank">
-							<p className="footer-logo fadeinLeft">SodaFactory</p>
-							</a>
-						</div>
-					</div>
-					<NewsletterForm email={this.state.email} />
-				</footer>
-			</div>
-			
-		);
-	}
+  render() {
+    return (
+      <div>
+        <footer className="footer" id="contact">
+          <div className="container footer-container">
+            <div className="content">
+              <p id="footer-title" className="footer-title fadeIn">
+                Contact Us
+              </p>
+              <p className="footer-p fadeIn">info@mega-mate.com</p>
+            </div>
+            <div className="content">
+              <a
+                className="logo-link"
+                href="https://www.facebook.com/megamate.sodafactory/"
+                target="_blank"
+              >
+                <p className="footer-logo fadeinLeft">Facebook</p>
+              </a>
+              <a
+                className="logo-link"
+                href="https://www.instagram.com/mega_mate/"
+                target="_blank"
+              >
+                <p className="footer-logo fadeinLeft">Instagram</p>
+              </a>
+              <a
+                className="logo-link"
+                href="https://www.sodafactory.se"
+                target="_blank"
+              >
+                <p className="footer-logo fadeinLeft">SodaFactory</p>
+              </a>
+            </div>
+          </div>
+          <NewsletterForm email={this.state.email} />
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default Footer;
