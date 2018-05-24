17da9219c7bc6ff75b2b49bd5175497c003c59a1
Keejv <Kaveh.haddad@chasacademy.se>
2018-05-24 15:12:30 +0200

    Fix import to uppercase

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,6 +12,6 @@ require("./bootstrap");
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require("./components/index");
+require("./components/Index");
 
 import Voucher from "./components/voucher/Index.js";
